7cb9c7fea7b87c3ea2493020fcc65d633ff377ba
dougpagani <redstonefreedom@gmail.com>
2017-04-06 18:28:39 -0400

    cleaned-up clutter files

---
+++ mySQL/Scripts/createScientist.sql
@@ -0,0 +1,14 @@
+# createScientist
+# http://stackoverflow.com/questions/16211107/how-to-execute-stored-procedure-in-another-stored-procedure-in-sql-server
+
+# IN(2) :: tuple(initials, 
+# describe scientists
+
+DELIMITER $$ 
+
+CREATE PROCEDURE createScientist()
+BEGIN
+
+
+END $$
+DELIMITER ;
--- mySQL/createDB.sql
+++
@@ -1,9 +0,0 @@
-### cramDB DUMMY-DATA
-
--- video
-insert into video values 
-(1101,null,null),
-(2987,null,null),
-(1623,null,null);
-
-
